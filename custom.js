14c2f4a7e2773dae43e4d78ba6c29e99c416449b
AYON <sarkerbishwendu90@gmail.com>
2025-05-04 10:06:50 +0600

    random number using do while loop

--- custom.js
+++ custom.js
@@ -1,4 +1,10 @@
-const minNum = 15;
-const maxNum = 100;
+let minNumber = 10;
+let maxNumber = 80;
 
-j
+let randomNumber;
+
+do {
+    randomNumber = Math.floor(Math.random() / 2 * (maxNumber + minNumber) -minNumber);
+} while (randomNumber < minNumber || randomNumber > maxNumber);
+
+console.log(randomNumber);
